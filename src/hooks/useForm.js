38ee69effbb17bf3bf1217cf7a94ac1b18c3f5a0
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-12-27 23:25:50 +0100

    added form validation with custom hook to form

---
+++ src/hooks/useForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { omit } from 'lodash';
+
+const useForm = (callback) => {
+
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+
+    if (Object.keys(errors).length === 0 && Object.keys(values).length !== 0) {
+      callback();
+    } else {
+      alert("Formuläret innehåller fel!")
+    }
+  };
+
+  const validate = (event, name, value) => {
+    switch (name) {
+      case 'firstName':
+        if (value.length < 2) {
+          setErrors({
+            ...errors,
+            'firstName': 'Ett namn måste ha minst 2 bokstäver!'
+          })
+        } else {
+          let newObj = omit(errors, "firstName");
+          setErrors(newObj);
+        }
+        break;
+      case 'surName':
+        if (value.length < 2) {
+          setErrors({
+            ...errors,
+            'surName': 'Ett namn måste ha minst 2 bokstäver!'
+          })
+        } else {
+          let newObj = omit(errors, "surName");
+          setErrors(newObj);
+        }
+        break;
+
+      case 'age':
+        if ((isNaN(value)) || value >= 18) {
+          setErrors({
+            ...errors,
+            age: 'Skriv in en siffra, mindre än 18!'
+          })
+        } else {
+          let newObj = omit(errors, "age");
+          setErrors(newObj)
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleChange = (event) => {
+    event.persist();
+
+    let name = event.target.name;
+    let val = event.target.value;
+
+    validate(event, name, val);
+
+    setValues({
+      ...values,
+      [name]: val,
+    })
+  }
+  return {
+    values,
+    errors,
+    handleChange,
+    handleSubmit
+  }
+};
+export default useForm;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,35 +1,55 @@
 import React from 'react';
 
+import useForm from '../hooks/useForm';
+
 const SignUp = () => {
+
+  //Final submit function
+  const formSubmit = () => {
+
+    console.log("Callback function when form is submitted!");
+    console.log("Form Values ", values);
+  }
+
+  const { handleChange, values, errors, handleSubmit } = useForm(formSubmit);
+
+
+
   return (
     <div>
       <h2>Anmälan</h2>
       <p>Var snäll och skicka ett eget svar för varje person som ska komma på festen, även barn. Glöm inte klicka på Skicka-knappen längst ned när du är klar.</p>
-      <form className="signup-form">
-        <label forHtml="firstName" className="form-label">Förnamn</label>
-        <input id="firstName" type="text" className="form-element" />
-        <label forHtml="SurName" className="form-label">Efternamn</label>
-        <input id="surName" type="text" className="form-element" />
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <label htmlFor="firstName" className="form-label">Förnamn</label>
+        <input id="firstName" name="firstName" type="text" className="form-element" onChange={handleChange} />
+        {
+          errors.firstName && <p className="error">{errors.firstName}</p>
+        }
+        <label htmlFor="surName" className="form-label">Efternamn</label>
+        <input id="surName" name="surName" type="text" className="form-element" onChange={handleChange} />
+        {
+          errors.surName && <p className="error">{errors.surName}</p>
+        }
         <p className="form-paragraph">Jag är allergisk mot</p>
-        <label forHtml="gluten"><input type="checkbox" id="gluten" name="gluten" value="gluten" />Gluten</label>
-        <label forHtml="lactose"><input type="checkbox" id="lactose" name="lactose" value="lactose" />Laktos</label>
-        <label forHtml="milk"><input type="checkbox" id="milk" name="milk" value="milk" />Mjölkprotein</label>
-        <label forHtml="peanuts"><input type="checkbox" id="peanuts" name="peanuts" value="peanuts" />Jordnötter</label>
-        <label forHtml="hazelnuts"><input type="checkbox" id="hazelnuts" name="hazelnuts" value="hazelnuts" />Hasselnötter</label>
-        <label forHtml="other"><input type="checkbox" id="other" name="other" value="other" />Annat, skriv nedan</label>
+        <label htmlFor="gluten"><input type="checkbox" id="gluten" name="gluten" value="gluten" />Gluten</label>
+        <label htmlFor="lactose"><input type="checkbox" id="lactose" name="lactose" value="lactose" />Laktos</label>
+        <label htmlFor="milk"><input type="checkbox" id="milk" name="milk" value="milk" />Mjölkprotein</label>
+        <label htmlFor="peanuts"><input type="checkbox" id="peanuts" name="peanuts" value="peanuts" />Jordnötter</label>
+        <label htmlFor="hazelnuts"><input type="checkbox" id="hazelnuts" name="hazelnuts" value="hazelnuts" />Hasselnötter</label>
+        <label htmlFor="other"><input type="checkbox" id="other" name="other" value="other" />Annat, skriv nedan</label>
         <input id="otherAllergy" type="text" className="form-element" />
         <p className="form-paragraph">Jag är</p>
-        <label forHtml="vegetarian"><input type="checkbox" id="vegetarian" name="vegetarian" value="vegetarian" />Vegetarian</label>
-        <label forHtml="vegan"><input type="checkbox" id="vegan" name="vegan" value="vegan" />Vegan</label>
+        <label htmlFor="vegetarian"><input type="checkbox" id="vegetarian" name="vegetarian" value="vegetarian" />Vegetarian</label>
+        <label htmlFor="vegan"><input type="checkbox" id="vegan" name="vegan" value="vegan" />Vegan</label>
 
         <p className="form-paragraph">Jag är</p>
-        <label forHtml="adult"><input type="radio" id="adult" name="age" value="adult" />Över 18 år</label>
-        <label forHtml="young"><input type="radio" id="young" name="age" value="young" />Annan ålder, skriv nedan</label>
-        <input id="childsAge" type="text" className="form-element" />
-
-
+        <label htmlFor="adult"><input type="radio" id="adult" name="age" value="adult" />Över 18 år</label>
+        <label htmlFor="young"><input type="radio" id="young" name="age" value="young" />Annan ålder, skriv nedan</label>
+        <input id="childsAge" name="age" type="text" className="form-element" onChange={handleChange} />
+        {
+          errors.age && <p className="error">{errors.age}</p>
+        }
         <input type="submit" value="SKICKA" className="submit-button" />
-
       </form>
     </div>
   )
